95fa32057ca2fb0218c8368be0bc4e8b9b7061fa
HH\gstthomas <tommy199469@gmail.com>
2018-01-24 17:49:32 +0800

    done with download and upload to s3

--- s3.py
+++ s3.py
@@ -1,23 +1,30 @@
 import boto3 , botocore, s3_config
+import pandas as pd
 
 
-# Create an S3 client
-s3  = boto3.resource(
-    's3',
-    aws_access_key_id=s3_config.AWS_ACCESS_KEY_ID,
-    aws_secret_access_key=s3_config.AWS_SECRET_ACCESS_KEY,
-    region_name=s3_config.AWS_DEFAULT_REGION
-)
+def upload_file(file_name):
+    # Create an S3 client
+    s3  = boto3.resource(
+        's3',
+        aws_access_key_id=s3_config.AWS_ACCESS_KEY_ID,
+        aws_secret_access_key=s3_config.AWS_SECRET_ACCESS_KEY,
+        region_name=s3_config.AWS_DEFAULT_REGION
+    )
+    object_acl = s3.ObjectAcl(s3_config.BUCKET_NAME,file_name)
+    response = object_acl.put(ACL='public-read')
+    return response
 
+def download_file():
+    s3  = boto3.resource(
+        's3',
+        aws_access_key_id=s3_config.AWS_ACCESS_KEY_ID,
+        aws_secret_access_key=s3_config.AWS_SECRET_ACCESS_KEY,
+        region_name=s3_config.AWS_DEFAULT_REGION
+    )
 
+    try:
+        s3.Bucket(s3_config.BUCKET_NAME).download_file("stock.xlsx", 'stock.xlsx')
+    except botocore.exceptions.ClientError as e:
+        return False
 
-
-#download 
-"""try:
-    s3.Bucket(s3_config.BUCKET_NAME).download_file("stock.xlsx", 'stock.xlsx')
-except botocore.exceptions.ClientError as e:
-    if e.response['Error']['Code'] == "404":
-        print("The object does not exist.")
-    else:
-        raise
-"""
+    return True
